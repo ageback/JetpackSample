42cde0281564be1e9fa98f8275e1def29fb205fa
Michael Lu <yeskky@gmail.com>
2021-08-08 18:51:11 +0800

    使用ViewModel，数据和视图分离

---
+++ app/src/main/java/com/ebookfrenzy/jetpacksample/ui/main/MainFragment.kt
@@ -0,0 +1,43 @@
+package com.ebookfrenzy.jetpacksample.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ebookfrenzy.jetpacksample.R
+import kotlinx.android.synthetic.main.main_fragment.*
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        resultText.text = viewModel.getResult().toString()
+
+        convertButton.setOnClickListener {
+            if (dollarText.text.isNotEmpty()) {
+                viewModel.setAmount(dollarText.text.toString())
+                resultText.text = viewModel.getResult().toString()
+            } else {
+                resultText.text = "No Value"
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/ebookfrenzy/jetpacksample/ui/main/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.ebookfrenzy.jetpacksample.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val usd_to_eu_rate = 0.74f
+    private var dollerText = ""
+    private var result: Float = 0f
+
+    fun setAmount(value: String) {
+        this.dollerText = value
+        result = value.toFloat() * usd_to_eu_rate
+    }
+
+    fun getResult(): Float? {
+        return result
+    }
+}
